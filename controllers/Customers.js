d823cf18c6afa577fea5988596c3729df00245a4
mutharajuvamsikrishna <130433935+mutharajuvamsikrishna@users.noreply.github.com>
2024-12-10 16:03:34 +0530

    css changs

--- controllers/Customers.js
+++ controllers/Customers.js
@@ -11,66 +11,76 @@ CUSTOMER.customers = async (req, res) => {
     });
 };
 
+
 CUSTOMER.customersList = async (req, res) => {
     try {
+        
         var query = {};
+
         query['role'] = 3;
 
-        // Define pagination
-        var start = parseInt(req.query.start) || 0;
-        var dataLimit = parseInt(req.query.length) || 10;
+        // array of columns that you want to show in table
+        columns = ['fullname'];
+        var start = req.query.start;
+        var dataLimit = req.query.length;
+        // check if global search is enabled and it's value is defined
+        if (typeof req.query.search !== 'undefined' && req.query.search.value != '') {
 
-        // Global search
-        if (req.query.search && req.query.search.value) {
+            // get global search value
             var text = req.query.search.value;
 
-            query['$or'] = [
-                { fullname: { $regex: text, $options: 'i' } },
-                { email: { $regex: text, $options: 'i' } },
-                { mobile: { $regex: text, $options: 'i' } }
-            ];
+            // iterate over each field definition to check whether search is enabled
+            // for that particular column or not. You can set search enable/disable
+            // in datatable initialization.
+            for (var i=0; i<req.query.columns.length; i++) {
+                requestColumn = req.query.columns[i];
+                column = columns[requestColumn.data];
+
+                // if search is enabled for that particular field then create query
+                if (requestColumn.searchable == 'true') {
+                    query[column] = {
+                        $regex: text,
+                    };
+                }
+            }
         }
+        await UserModel.find(query).skip(start).limit(dataLimit).sort({ _id : 'desc' }).then(async (result)=>{
+            var mytable = {
+                draw:req.query.draw,
+                recordsTotal:0,
+                recordsFiltered:0,
+                data:[],
+            }
 
-        // Fetch data
-        const result = await UserModel.find(query)
-            .skip(start)
-            .limit(dataLimit)
-            .sort({ _id: -1 });
+            mytable.recordsTotal    = await UserModel.countDocuments(query);
+            mytable.recordsFiltered = await UserModel.countDocuments(query);
 
-        // Prepare response
-        let users = [];
-        for (const element of result) {
-            let totalOrder = await orderProducts.countDocuments({ order_uid: element._id });
+           if(result.length > 0){
+                for(const [key,element] of Object.entries(result)) {
 
-            users.push({
-                _id: element._id,
-                fullname: element.fullname,
-                role: element.role,
-                email: element.email,
-                mobile: element.mobile,
-                password: element.password,
-                profile_image: element.profile_image,
-                address: element.address,
-                city: element.city,
-                postal_code: element.postal_code,
-                country: element.country,
-                state: element.state,
-                gst_no: element.gst_no,
-                status: element.status,
-                verify_otp: element.verify_otp,
-                resetPasswordToken: element.resetPasswordToken,
-                firebase_token: element.firebase_token,
-                createdAt: element.createdAt,
-                updatedAt: element.updatedAt,
-                totalOrder: totalOrder
-            });
-        }
+                    let totalOrder = await orderProducts.countDocuments({order_uid : element._id });
+                    
+                    mytable.data[key] = [ ++start,
+                                          element.fullname,
+                                          element.email,
+                                          element.mobile,
+                                          totalOrder,
+                                          helper.statusLable[element.status],
+                                          //`<!--a href="javascript:void(0);" title="View" class="viewCustomer" data-cust-id="${element._id}"><i class="fas fa-eye"></i></a>
+                                         // <a href="javascript:void(0);" title="Delete" class="deleteRecords" data-delete-id="${element._id}" data-url="/dashboard/delete_customer"><i class="fas fa-trash-alt"></i></a-->`
+                                        ];
+                }; 
 
-        res.status(200).json(users); 
-    } catch (err) {
-        res.status(500).json({ status: 0, message: 'Error: ' + err.message });
-    }
-};
+                res.status(200).json(mytable);
+                  
+            } else {
+                res.status(200).json(mytable);
+            }
+        });
+      } catch (err) {
+            res.status(401).json({ status : 0, message : 'error '+ err });
+      }
+  };
 
 
   CUSTOMER.deleteCustomer = async (req, res) => {
